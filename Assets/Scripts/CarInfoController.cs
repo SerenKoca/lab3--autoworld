0ea7ee8b45d847e2303fe8180a357cd31d6aca31
Ben Devos <bendevos07@gmail.com>
2024-12-19 10:07:45 +0100

    fix: bug with image display car info

--- Assets/Scripts/CarInfoController.cs
+++ Assets/Scripts/CarInfoController.cs
@@ -37,9 +37,9 @@ public class CarInfoController : MonoBehaviour
 
             case 2:
                 titleText.text = "Ford Model 40 / V8 (1933)";
-                descriptionText.text = "De Ford Model 40 / V8, geïntroduceerd in 1933, was de eerste productieauto met een V8-motor van Ford. Dit model verbeterde de prestaties aanzienlijk ten opzichte van eerdere modellen. Het had een topsnelheid van 122 km/u en was uitgerust met een V8-motor die 65 pk produceerde. De lengte van het voertuig was 4,630 meter. De specificaties voor 0-45 mph en 0-60 mph zijn niet beschikbaar, maar de acceleratie van de Model 40 / V8 was sterk voor zijn tijd, met een versnelling van 1,34 m/s².";
+                descriptionText.text = "De Ford Model 40 / V8, geïntroduceerd in 1933, was de eerste productieauto met een V8-motor van Ford. Dit model verbeterde de prestaties aanzienlijk ten opzichte van eerdere modellen. Het had een topsnelheid van 122 km/u en was uitgerust met een V8-motor die 65 pk produceerde. De lengte van het voertuig was 4,630 meter. De specificaties voor 0-45 mph en 0-60 mph zijn niet beschikbaar, maar de acceleratie van de Model 40 / V8 was sterk voor zijn tijd, met een versnelling van 1,34 m/s².";                             
+                carImage.material.mainTexture = car2Image; // Texture voor de eerste auto
                 break;
-
             case 3:
                 titleText.text = "Ford Mustang Mk1 (1965)";
                 descriptionText.text = "De Ford Mustang Mk1, gelanceerd in 1965, is een iconisch model dat het sportwagensegment in de VS revolutioneerde. De Mustang Mk1 had een topsnelheid van 181 km/u en een 1/4 mijl tijd van 16,4 seconden. Het had een krachtige motor die 225 pk produceerde, en de lengte van de auto was 4,613 meter. De Mustang Mk1 was uitzonderlijk in zijn acceleratie van 0-45 mph en 0-60 mph, hoewel de exacte tijden niet beschikbaar zijn. De acceleratie van de Mustang Mk1 was 3,16 m/s², wat hem tot een van de snelste auto's van zijn tijd maakte.";
